25259d32c7aeb001d2faf22a6ea4dff760e73cc6
Azule2 <69428406+Azule2@users.noreply.github.com>
2021-12-23 11:52:58 +0300

    main.dart
    New Course Dart Flutter Button

---
+++ main.dart
@@ -0,0 +1,12 @@
+ body:  Center(
+        // ignore: deprecated_member_use
+        child: RaisedButton(
+          onPressed: () {
+            // ignore: avoid_print
+            print("object");
+          },
+          child: const Text("Click"),
+          color: Colors.red,
+          textColor: Colors.white,
+        ),
+      ),
